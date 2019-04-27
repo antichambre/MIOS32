1f4a78fe8a554dde42ad700bdbecf9507beb07ac
antichambre <bdupeyron@gmail.com>
2019-04-27 09:42:40 +0200

    ili9361 commit

---
+++ modules/app_lcd/ili9361/app_lcd.mk
@@ -0,0 +1,19 @@
+# $Id: app_lcd.mk 1222 2011-06-23 21:12:04Z tk $
+# defines additional rules for application specific LCD driver
+
+# enhance include path
+C_INCLUDE +=	-I $(MIOS32_PATH)/modules/app_lcd/ili9361
+
+# add modules to thumb sources
+THUMB_SOURCE += \
+	$(MIOS32_PATH)/modules/app_lcd/ssd1322/app_lcd.c
+
+# include fonts
+include $(MIOS32_PATH)/modules/glcd_font/glcd_font.mk
+
+# include fonts
+include $(MIOS32_PATH)/modules/glcd_font/glcd_font_4bit.mk
+
+# directories and files that should be part of the distribution (release) package
+DIST += $(MIOS32_PATH)/modules/app_lcd/ssd1322
+
